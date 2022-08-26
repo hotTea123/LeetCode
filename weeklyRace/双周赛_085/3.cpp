3f830ac83ba0b1acdc5b3e9e5d3dbf063f95004c
hotTea123 <958436561@qq.com>
2022-08-27 02:36:23 +0800

    双周赛_085

--- weeklyRace/双周赛_085/3.cpp
+++ weeklyRace/双周赛_085/3.cpp
@@ -1,2 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Solution {
+private:
+    vector<char> letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+            'q','r','s','t','u','v','w','x','y','z'};
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        vector<int> diff(s.size(), 0);
+        for(int i=0;i < shifts.size();i++){
+            int num = shifts[i][2] == 0 ? -1:1;
+            diff[shifts[i][0]] += num;
+            if(shifts[i][1]+1 < s.size())
+                diff[shifts[i][1]+1] -= num;
+        }
+        int sum = 0;
+        for(int i=0;i < s.size();i++){
+            sum += diff[i];
+            s[i] = (s[i]-'a'+sum) % 26;
+        }
+        return s;
+    }
+};
+
+int main(){
+    string s = "ksztajnymer";
+    vector<vector<int>> shifts = {{4,9,1},{2,5,1},{7,9,0},{6,10,0},{1,7,1},{0,7,1},{0,3,0},{3,3,1},{7,9,0},{7,7,0},{6,7,0},{7,9,0},{3,9,0},{10,10,1},{8,9,0},{6,9,1},{1,2,1},{3,9,0},{8,10,1},{4,7,1},{9,10,1},{8,9,0},{5,8,0},{8,9,1},{0,7,1},{2,2,1},{8,8,1},{3,7,0},{1,10,1},{9,9,1},{4,9,0},{5,6,1},{7,8,1},{8,9,0},{7,7,1},{9,9,1}};
+    auto res = Solution().shiftingLetters(s, shifts);
+    return 0;
+}
--- weeklyRace/双周赛_085/4.cpp
+++ weeklyRace/双周赛_085/4.cpp
@@ -1,2 +1,67 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class UnionFindSet{
+private:
+    vector<int> parent;
+    vector<int> nums;
+    vector<long long> sum;
+    long long maxSum;
+public:
+    UnionFindSet(int n){
+        parent = vector<int>(n);
+        nums = vector<int>(n, 0);
+        sum = vector<long long>(n);
+        maxSum = 0;
+        for(int i=0;i < n;i++)
+            parent[i] = i;
+    }
+
+    //返回当前节点的根节点
+    int Find(int x){
+        if(parent[x] != x)
+            parent[x] = Find(parent[x]);
+        return parent[x];
+    }
+
+    //连接x，y
+    void Union(int x, int y){
+        int xParent = Find(x), yParent = Find(y);
+        if(xParent == yParent)
+            return;
+        parent[xParent] = yParent;
+        sum[yParent] += sum[xParent];
+        maxSum = max(maxSum, sum[yParent]);
+    }
+
+    void add(int idx, long long num){
+        nums[idx] = num;
+        sum[idx] = num;
+        if(idx > 0 && nums[idx-1] != 0)
+            Union(idx, idx-1);
+        if(idx < nums.size()-1 && nums[idx+1] != 0)
+            Union(idx, idx+1);
+        maxSum = max(maxSum, num);
+    }
+
+    long long getMaxsum(){
+        return maxSum;
+    }
+};
+
+class Solution {
+public:
+    vector<long long> maximumSegmentSum(vector<int>& nums, vector<int>& removeQueries) {
+        int n = nums.size();
+        vector<bool> isVisit(n, false);
+        UnionFindSet ufs(n);
+        vector<long long> res(n);
+        res[n-1] = 0;
+        for(int i=n-1;i > 0;i--){
+            int idx = removeQueries[i];
+            ufs.add(idx, nums[idx]);
+            res[i-1] = ufs.getMaxsum();
+        }
+        return res;
+    }
+};
