0cf523011676f87411ffce1a19cceec6aaa72093
hotTea123 <958436561@qq.com>
2022-08-28 00:12:38 +0800

    2022.8.27

--- weeklyRace/周赛_307/1.cpp
+++ weeklyRace/周赛_307/1.cpp
@@ -1,2 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    int minNumberOfHours(int initialEnergy, int initialExperience, vector<int>& energy, vector<int>& experience) {
+        int res = 0;
+        for(int i=0;i < experience.size();i++){
+            if(initialEnergy <= energy[i]){
+                res += energy[i] - initialEnergy+1;
+                initialEnergy = energy[i] + 1;
+            }
+            initialEnergy -= energy[i];
+            if(initialExperience <= experience[i]){
+                res += experience[i] - initialExperience +1;
+                initialExperience = experience[i]+1;
+            }
+            initialExperience += experience[i];
+        }
+        return res;
+    }
+};
--- weeklyRace/周赛_307/2.cpp
+++ weeklyRace/周赛_307/2.cpp
@@ -1,2 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Solution {
+public:
+    string largestPalindromic(string num) {
+        bool isExsit = false;
+        vector<int> cnt(10, 0);
+        for(int i=0;i < num.size();i++){
+            ++cnt[num[i]-'0'];
+            if(num[i]-'0' != 0 && cnt[num[i]-'0'] == 2)
+                isExsit = true;
+        }
+        string s = "";
+        int maxNum = -1;
+        for(int i=9;i >=0;i--){
+            while(cnt[i] >=2 ){
+                s += '0'+i;
+                cnt[i] -= 2;
+            }
+            if(cnt[i] != 0)
+                maxNum = max(maxNum, i);
+        }
+        if(!isExsit)
+            return to_string(max(maxNum, 0));
+        string sRev = s;
+        reverse(sRev.begin(), sRev.end());
+        if(maxNum == -1)
+            return s+sRev;
+        return s+to_string(maxNum)+sRev;
+    }
+};
+
+int main(){
+    string num = "000119";
+    auto res = Solution().largestPalindromic(num);
+    return 0;
+}
